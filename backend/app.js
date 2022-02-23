ae54f4d984e6f3a94ef31aa1fb0e0fa38781d933
Viruuus30 <71880004+Viruuus30@users.noreply.github.com>
2022-02-23 18:32:42 +0100

    main

--- backend/app.js
+++ backend/app.js
@@ -13,20 +13,20 @@ app.use(bodyParser.json());
 //const userRoutes = require('./routes/user');
 
 
-const app = express();
+//const app = express();
 //Middleware pour toutes les routes
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+//app.use((req, res, next) => {
+ //   res.setHeader('Access-Control-Allow-Origin', '*');
+ //   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+ //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+ //   next();
+//});
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+//app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+//app.use(bodyParser.urlencoded({
+// extended: true
+//}));
 
 
 //app.use('/api/comment', commentRoutes);
